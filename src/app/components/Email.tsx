b0d9b0016289c290061731cc5090f8d2eeea5e4b
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2024-10-26 23:50:38 +0500

    complete

--- src/app/components/Email.tsx
+++ src/app/components/Email.tsx
@@ -5,7 +5,7 @@ function Email() {
       <div className="bg-[#202426] p-12 space-y-2">
         <h1 className="text-[#868381] text-center">LET&apos;S TALK</h1>
         <h1 className="text-center text-white md:text-[2vw] text-[4vw] font-bold">
-          ranasubhanrajput6677@gmail.com
+          abdusubhan6678@gmail.com
         </h1>
       </div>
     </>
